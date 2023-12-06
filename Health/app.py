b5f430e4d9dc5b9cbdadb7ba0538ab41f19f5db4
Vilmor Somera <vsomera77@outlook.com>
2023-12-05 18:34:48 -0800

    updated json for health checks

--- Health/app.py
+++ Health/app.py
@@ -3,6 +3,7 @@ import threading
 import time
 import json
 from flask import Flask, jsonify
+import yaml
 
 # Configuration and logging setup
 
@@ -17,11 +18,21 @@ service_statuses = {
     "last_update": ""
 }
 
+with open("./app_conf.yml", 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
 def poll_services():
     while True:
-        # Logic to poll each service and update service_statuses
-        # Use requests.get with a timeout of 5 seconds
-        pass
+        for service_name, service_info in app_config['service'].items():
+            try:
+                response = requests.get(service_info['url'], timeout=5)
+                # Update the status based on the response code
+                service_statuses[service_name] = "Up" if response.status_code == 200 else "Down"
+            except requests.RequestException:
+                service_statuses[service_name] = "Down"
+
+        # Update last_update time
+        service_statuses["last_update"] = time.strftime("%Y-%m-%d %H:%M:%S")
         time.sleep(20)
 
 @app.route('/health', methods=['GET'])
@@ -32,5 +43,4 @@ def get_service_statuses():
 if __name__ == '__main__':
     # Start the polling thread
     threading.Thread(target=poll_services, daemon=True).start()
-    
     app.run(host='0.0.0.0', port=8120)
